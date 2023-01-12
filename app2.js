9c9dacb71950cdc116e9b6111864ef24069f3cee
johnnyjvang <johnnyjvang@gmail.com>
2023-01-12 00:11:34 -0800

    sounds
    added sound and cards

--- app2.js
+++ app2.js
@@ -1,6 +1,13 @@
 var slideIndex = 1;
 showDivs(slideIndex);
 
+let animals = {
+  english:["bear", "bull", "cat", "chicken", "cow", "deer", "duck", "elephant", "fox", "goat", "hen", "horse", "lion", "monkey"],
+  white:["dais", "heev nyuj", "miv", "qaib", "nyuj", "muas lwj", "os", "ntxhw", "hma", "tshis", "poj qaib", "nees", "tsov ntxhuav", "liab"],
+  green:["dlais", "heev nyuj", "miv", "qab", "nyuj", "mos lwj", "os", "ntxhw", "hma", "mi es", "puj qaib", "neeg", "tsuv ntxhuav", "lab"],
+  flashcard_name:"animals"
+}
+
 function plusDivs(n) {
   showDivs(slideIndex += n);
 }
@@ -24,3 +31,23 @@ function showDivs(n) {
   x[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " w3-red";
 }
+
+// this creates white hmong
+// need to create the one for green hmong as well, easy to have an if/else statement 
+function create_sound(category_list) {
+  for (let i = 0; i < category_list.english.length; i++) {
+
+    let sound_category = category_list.flashcard_name;
+    let sound_name = category_list.english[i];
+    let audio_link = "sounds/" + sound_category + "/" + sound_name + ".mp3";
+
+    document.querySelectorAll('.sound_element')[i].addEventListener("click", function() {
+      // console.log("added click function to" + String(i));
+      let voice_recording = new Audio(audio_link);
+       // console.log(audio_link)
+      voice_recording.play();
+    });
+  }
+}
+
+create_sound(animals)
